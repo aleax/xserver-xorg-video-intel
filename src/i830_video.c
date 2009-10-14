1dceba2603969870ee87ea4b467e454fff6ab18f
Daniel Vetter <daniel.vetter@ffwll.ch>
2009-10-14 18:09:08 +0200

    Xv overlay: fix planar YUV copy for right rotated crtcs
    While copying and rotating the buffer, array access was out of bounds when
    rotated to the right (RR_Rotate_270).  My buffer handling changes probably
    made this bug much more likely to actually result in a SIGSEGV.
    
    I've checked the logs and the bug exists since rotation has been supported,
    i.e.  this looks like a candidate for cherry-picking for all supported
    releases.
    
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Eric Anholt <eric@anholt.net>
    (manually cherry-picked from commit aaedeffe00d9414bb03723dbc30b4938a07ce5fa)

--- src/i830_video.c
+++ src/i830_video.c
@@ -1502,10 +1502,10 @@ I830CopyPackedData(I830PortPrivPtr pPriv,
 	    for (j = 0; j < w; j+=2) {
 		/* Copy U */
 		dst[(((h - i)*2) - 3) + (j * dstPitch)] = src[(j*2) + 1 + (i * srcPitch)];
-		dst[(((h - i)*2) - 3) + ((j - 1) * dstPitch)] = src[(j*2) + 1 + ((i+1) * srcPitch)];
+		dst[(((h - i)*2) - 3) + ((j + 1) * dstPitch)] = src[(j*2) + 1 + ((i+1) * srcPitch)];
 		/* Copy V */
 		dst[(((h - i)*2) - 1) + (j * dstPitch)] = src[(j*2) + 3 + (i * srcPitch)];
-		dst[(((h - i)*2) - 1) + ((j - 1) * dstPitch)] = src[(j*2) + 3 + ((i+1) * srcPitch)];
+		dst[(((h - i)*2) - 1) + ((j + 1) * dstPitch)] = src[(j*2) + 3 + ((i+1) * srcPitch)];
 	    }
 	}
 	break;
